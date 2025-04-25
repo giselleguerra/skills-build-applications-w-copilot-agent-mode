8b3058ad0afac491fcd3f0d3c1bc4cc385b4616b
Gi Guerra <87152685+giselleguerra@users.noreply.github.com>
2025-04-25 00:28:14 +0000

    Atualizações para reconfigurar o banco de dados com djongo como engine

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -76,18 +76,14 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Configuração do MongoDB usando pymongo
+# Configuração do banco de dados usando djongo
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.dummy",  # Dummy engine para evitar erros do Django
+        "ENGINE": "djongo",
+        "NAME": "octofit_db",
+        "HOST": "localhost",
+        "PORT": 27017,
     }
-}
-
-# Corrigindo o tipo da porta para inteiro
-MONGO_DB_SETTINGS = {
-    "NAME": "octofit_db",
-    "HOST": "localhost",
-    "PORT": 27017,  # Certificando-se de que é um número inteiro
 }
 
 
